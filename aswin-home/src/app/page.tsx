3c8265b0cfd6927ee60a53f114f3439e723d1f85
Aswin Rajeev <aswin@aswinrajeev.com>
2024-03-02 22:25:06 +0530

    New layout for the home page

--- aswin-home/src/app/page.tsx
+++ aswin-home/src/app/page.tsx
@@ -2,9 +2,18 @@ import { Fragment } from "react";
 
 export default function Home() {
   return (
-    <Fragment>
-      <h1>Aswin Rajeev</h1>
-      <p>Hi, my name is Aswin Rajeev and I am a Software Development Engineer (SDE II) at Amazon.</p>
-    </Fragment>
+    <div className="about">
+      <div className="display-pic">
+        <img src="/dp.jpg"/>
+      </div>
+      <div className="hello">Hello</div>
+      <div className="tagline">I am a Software Development Engineer</div>
+      <div className="intro">I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you.</div>
+      <div className="social">
+        <a href="https://blog.aswin.me"><li><i className="bi bi-medium"></i> Blog at Medium</li></a>
+        <a href="https://www.linkedin.com/in/aswinrajeev"><li><i className="bi bi-linkedin"></i> LinkedIn Profile</li></a>
+        <a href="https://www.github.com/aswinrajeev"><li><i className="bi bi-github"></i> Projects</li></a>
+      </div>
+    </div>
   );
 }
