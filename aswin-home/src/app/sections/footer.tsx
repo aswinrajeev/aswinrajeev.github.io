5e9955f753005b4a616754367f1ebd834a4d8736
Aswin Rajeev <aswin@aswinrajeev.com>
2024-03-10 16:19:38 +0530

    Update project structure

---
+++ aswin-home/src/app/sections/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <span>Copyright © Aswin Rajeev, 2021- 2024</span>
+        </footer>
+    );
+}
---
+++ aswin-home/src/app/sections/header.tsx
@@ -0,0 +1,16 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <a href="/"><img className="logo clickable adaptive" src="/aswinrajeev.webp" alt="Aswin Rajeev logo"/></a>
+            <nav className="navbar">
+                <ul className="nav-list">
+                    <a href="/"><li className="nav-link">Home</li></a>
+                    <a href="https://blog.aswin.me"><li className="nav-link">Blog</li></a>
+                    <a href="#"><li className="nav-link">Works</li></a>
+                    <a href="#"><li className="nav-link">About</li></a>
+                    <a href="#"><li className="nav-link">Contact</li></a>
+                </ul>
+            </nav>
+      </header>
+    );
+}
