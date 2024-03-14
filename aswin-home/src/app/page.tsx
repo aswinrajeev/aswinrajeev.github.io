296929bb46c7d138539cdb1eb48b4e0bed4f9aec
Aswin Rajeev <aswin@aswinrajeev.com>
2024-03-14 16:41:45 +0530

    Add partnerships section

--- aswin-home/src/app/page.tsx
+++ aswin-home/src/app/page.tsx
@@ -2,12 +2,14 @@ import { Fragment } from "react";
 import About from "./sections/about";
 import Header from "./sections/header";
 import Footer from "./sections/footer";
+import Affiliations from "./sections/affiliations";
 
 export default function Home() {
   return (
     <Fragment>
       <Header/>
       <About/>
+      <Affiliations/>
       <Footer/>
     </Fragment>
   );
