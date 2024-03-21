a0a40ecba83ee61073e48dcd1b58fd21a81040f9
Aswin Rajeev <aswin@aswin.me>
2024-03-21 16:37:16 +0530

    Add favicon

--- aswin-home/src/app/layout.tsx
+++ aswin-home/src/app/layout.tsx
@@ -18,6 +18,10 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="64x64" href="/favicon/favicon-64x64.png"/>
+        <link rel="icon" type="image/png" sizes="128x128" href="/favicon/favicon-128x128.png"/>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
